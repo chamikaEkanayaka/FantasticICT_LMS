e409740e208b08ff358e80557f6bc64a85c43912
chamikaEkanayaka <chamikaekanayaka@icloud.com>
2023-07-16 21:31:47 +0530

    Nav Bar Edited

--- fict_client/src/components/NavBar.js
+++ fict_client/src/components/NavBar.js
@@ -1,9 +1,21 @@
 import React from "react";
+import styles from "../components/NavBarStyles.module.css";
+import fictIcon from "../Images/fantasticIctLogo.png";
 
-function NavBar(){
-    <>
-        Hello
-    </>
+function NavBar() {
+  return (
+    <div className={styles.navBar}>
+      <div className={styles.navBarLeft}>
+        <img src={fictIcon} alt="fantasticIctLogo" width="120px"/>
+      </div>
+      <div className={styles.navBarRight}>
+        <div className={styles.navBarTab}>Home</div>
+        <div className={styles.navBarTab}>Study</div>
+        <div className={styles.navBarTab}>About</div>
+        <div className={styles.navBarTab}>Contact</div>
+      </div>
+    </div>
+  );
 }
 
 export default NavBar;
